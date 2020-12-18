22b5f64c0a2d672c349820678529a1ff7a5ae55a
ctysingh <chaitanyasingh@gmail.com>
2020-12-18 09:46:43 -0500

    adding initial state for product

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -37,12 +37,8 @@ export class ProductListComponent implements OnInit, OnDestroy {
     });
 
     this.store.select('products').subscribe(
-      products => {
-        if(products){
-          this.displayCode = products.showProductCode;
-        }
-      }
-    )
+      products => this.displayCode = products.showProductCode
+    );
   }
 
   ngOnDestroy(): void {
