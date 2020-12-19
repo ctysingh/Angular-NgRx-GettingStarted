da9457b25fb7d28515c26547650124026f4bd91f
ctysingh <chaitanyasingh@gmail.com>
2020-12-19 07:44:10 -0500

    add selector for product feature

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,10 +1,28 @@
-import { createAction, createReducer, on } from "@ngrx/store";
+import { createAction, createFeatureSelector, createReducer,
+          createSelector, on } from "@ngrx/store";
 import * as AppState from "../../state/app.state";
 import { Product } from "../product";
 
 export interface State extends AppState.State {
   products: ProductState;
 }
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
 
 export interface ProductState {
   showProductCode: boolean;
