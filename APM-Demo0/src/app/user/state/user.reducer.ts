23c5c58da15ad9d9dc20941df7410051fe102941
ctysingh <chaitanyasingh@gmail.com>
2020-12-19 08:29:10 -0500

    Add user state

--- APM-Demo0/src/app/user/state/user.reducer.ts
+++ APM-Demo0/src/app/user/state/user.reducer.ts
@@ -1,8 +1,26 @@
 import { createAction, createReducer, on } from "@ngrx/store";
+import * as AppState from "../../state/app.state";
+import { User } from "../user";
 
-export const userReducer = createReducer(
- { maskUserName: true },
- on(createAction('[User] Mask User Name'), state => {
+export interface state extends AppState.State {
+  users: UserState;
+}
+
+export interface UserState {
+  maskUserName: boolean;
+  currentUser: User;
+  users: User[];
+}
+
+const initialState: UserState = {
+  maskUserName: true,
+  currentUser: null,
+  users: null
+}
+
+export const userReducer = createReducer<UserState>(
+  initialState,
+ on(createAction('[User] Mask User Name'), (state): UserState => {
    console.log('User masked changed :' + JSON.stringify(state));
   return {
     ...state,
