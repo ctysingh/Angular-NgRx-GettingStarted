9e1fec57ef37f681e00f7cbd0bab25182f99ed7e
ctysingh <chaitanyasingh@gmail.com>
2020-12-31 14:36:04 -0500

    adding effects for create action

--- APM-Demo0/src/app/products/state/product.effects.ts
+++ APM-Demo0/src/app/products/state/product.effects.ts
@@ -32,6 +32,17 @@ export class ProductEffects {
     )
   })
 
+  createProduct$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(ProductActions.createProduct),
+      concatMap(action => this.productService.createProduct(action.product)
+      .pipe(
+        map(product => ProductActions.createProductSuccess({ product })),
+        catchError(error => of(ProductActions.createProductFailure({ error })))
+      ))
+    )
+  })
+
   deleteProduct$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ProductActions.deleteProduct),
